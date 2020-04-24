4999c8ae451b27e50a061905c5c08aedfe8b524d
sonam80 <58426232+sonam80@users.noreply.github.com>
2020-04-24 12:30:38 +0530

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ echo "hi";
 $host       = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "test";
+$dbname     = "service";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
